3c501e81007402264269bfce96dd79ef3999a120
c0ldheart <thecoldheart@foxmail.com>
2023-12-21 19:16:49 +0800

    ✨ feat(as01): code change

--- 1-shell/ferris-wheel/compile-pass/derive.rs
+++ 1-shell/ferris-wheel/compile-pass/derive.rs
@@ -1,4 +1,7 @@
 // FIXME: Make me compile! Diff budget: 1 line.
+// 在 Rust 中，{:?} 是用于调试输出的格式化打印指令。它可以用于打印出结构体、枚举、数组等类型的调试信息。
+//  #[derive(Debug)] 注解自动实现了 Debug trait
+#[derive(Debug, Clone, Copy)]
 enum Duration {
     MilliSeconds(u64),
     Seconds(u32),
--- 1-shell/ferris-wheel/compile-pass/feature-1.rs
+++ 1-shell/ferris-wheel/compile-pass/feature-1.rs
@@ -1,5 +1,7 @@
 // FIXME: Make me compile! Diff budget: 2 lines.
 // Do not modify this definition.
+// 在 Rust 1.26 版本及更高版本中，i128 类型已经成为稳定特性
+#![feature(i128_type)] 
 enum Duration {
     MicroSeconds(u128),
     MilliSeconds(u64),
